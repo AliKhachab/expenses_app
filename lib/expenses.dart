536487365a69f792437067fa4fa27bf8a01c700b
AliKhachab <a.khachab620@gmail.com>
2025-10-09 12:03:04 -0400

    dummy data created

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,10 +11,21 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+
+  final List<Expense> _registeredExpenses = [
+    Expense(title: "Cheeseburger", amount: 12.45, date: DateTime.now(), category: Category.food),
+    Expense(title: "Spider-Man Movie", amount: 15.00, date: DateTime.now(), category: Category.leisure),
+    Expense(title: "Office Supplies", amount: 75.23, date: DateTime.now(), category: Category.work),
+    Expense(title: "Flight to London", amount: 250.36, date: DateTime.now(), category: Category.travel),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Expenses"),
+    return Scaffold(
+      body: Column(children: const [
+        Text("Chart"),
+        Text("Expense List")
+      ]),
     );
   }
 }
