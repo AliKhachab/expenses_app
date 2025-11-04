4444b2c845315e855f8ce9103d6fb211a667cfa5
AliKhachab <a.khachab620@gmail.com>
2025-11-04 11:40:04 -0500

    made chartbar

---
+++ lib/widgets/chart/chart_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  const ChartBar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode = MediaQuery.of(context).platformBrightness == Brightness.dark;
+
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 4.0),
+        child: FractionallySizedBox(
+          heightFactor: 0.6,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              //color: isDarkMode ? Colors.tealAccent : Colors.teal,
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(8)),
+              shape: BoxShape.rectangle,
+              color: isDarkMode ? Theme.of(context).colorScheme.secondary : Theme.of(context).colorScheme.primary,
+            ),
+          )
+        ),
+      )
+    );
+  }
+
+}
