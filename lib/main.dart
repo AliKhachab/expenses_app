3a693352359b5645686c5e1d0538b12616ffa84c
AliKhachab <a.khachab620@gmail.com>
2025-11-04 12:29:59 -0500

    theme comment for next class in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,8 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Expenses()));
+  runApp(MaterialApp(
+    home: Expenses(),
+    //theme: ThemeData().copyWith(scaffoldBackgroundColor: Color.fromARGB(225, 220, 189, 252))
+  ));
 }
