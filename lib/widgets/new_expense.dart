ac6a64df84a9d4f56e54a01808cdb9998807c1cc
AliKhachab <a.khachab620@gmail.com>
2025-11-18 11:46:32 -0500

    added dialog based on platform

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/cupertino.dart';
+
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 final formatter = DateFormat.yMd();
 
@@ -23,6 +27,59 @@ class _NewExpenseState extends State<NewExpense> {
   DateTime? _selectedDate;
   Category _selectedCategory = Category.leisure;
 
+  void _showDialog(String message) {
+    showPlatformDialog(
+      context: context,
+      builder: (_) => PlatformAlertDialog(
+        title: Text("Invalid Input"),
+        content: Text(message),
+        actions: [
+          PlatformDialogAction(
+            child: Text("OK"),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+  // void _showDialog(String message) {
+  //   if (Platform.isIOS) {
+  //     showCupertinoDialog(
+  //       context: context,
+  //       builder: (context) => CupertinoAlertDialog(
+  //         title: Text("Invalid Input"),
+  //         content: Text(message),
+  //         actions: [
+  //           CupertinoDialogAction(
+  //             onPressed: () {
+  //               Navigator.pop(context);
+  //             },
+  //             child: Text("OK"),
+  //           ),
+  //         ],
+  //       ),
+  //     );
+  //     return;
+  //   }
+  //   showDialog(
+  //     context: context,
+  //     builder: (context) => AlertDialog(
+  //       title: Text("Invalid Input"),
+  //       content: Text(message),
+  //       actions: [
+  //         TextButton(
+  //           onPressed: () {
+  //             Navigator.pop(context);
+  //           },
+  //           child: Text("OK"),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -30,36 +87,19 @@ class _NewExpenseState extends State<NewExpense> {
     super.dispose();
   }
 
-  void _showErrorDialogue(String message) {
-    showDialog(context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Invalid Input"),
-        content: Text(message),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text("OK"),
-          ),
-        ],
-      ),
-    );
-  }
-
   void _submitExpenseData() {
     if (_titleController.text.trim().isEmpty) {
-      _showErrorDialogue("Please enter a valid title.");
+      _showDialog("Please enter a valid title.");
       return;
     }
     final enteredAmnt = double.tryParse(_amountController.text);
     if (enteredAmnt == null || enteredAmnt <= 0) {
-      _showErrorDialogue("Please enter a valid amount.");
+      _showDialog("Please enter a valid amount.");
       return;
     }
 
     if (_selectedDate == null) {
-      _showErrorDialogue("Please choose a date.");
+      _showDialog("Please choose a date.");
       return;
     }
 
@@ -90,9 +130,16 @@ class _NewExpenseState extends State<NewExpense> {
 
   @override
   Widget build(BuildContext context) {
-    final keyboardSpace = MediaQuery.of(context).viewInsets.bottom; // Get the space taken by the keyboard
+    final keyboardSpace = MediaQuery.of(
+      context,
+    ).viewInsets.bottom; // Get the space taken by the keyboard
     return Padding(
-      padding: EdgeInsets.fromLTRB(16.0, 48.0, 16.0, keyboardSpace + 16.0), // Add padding at the bottom equal to keyboard space so that content is not hidden
+      padding: EdgeInsets.fromLTRB(
+        16.0,
+        48.0,
+        16.0,
+        keyboardSpace + 16.0,
+      ), // Add padding at the bottom equal to keyboard space so that content is not hidden
       child: Column(
         children: [
           TextField(
@@ -121,12 +168,16 @@ class _NewExpenseState extends State<NewExpense> {
               const SizedBox(height: 16),
               Expanded(
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end, // Align to the right
-                  crossAxisAlignment: CrossAxisAlignment.center, // Center vertically
+                  mainAxisAlignment:
+                      MainAxisAlignment.end, // Align to the right
+                  crossAxisAlignment:
+                      CrossAxisAlignment.center, // Center vertically
                   children: [
-                    Text(_selectedDate == null
-                        ? "No Date Chosen"
-                        : formatter.format(_selectedDate!)), // note to self: ! means "this nullable variable isnt null, trust me", so dart wont throw potential null exceptions/errors
+                    Text(
+                      _selectedDate == null
+                          ? "No Date Chosen"
+                          : formatter.format(_selectedDate!),
+                    ), // note to self: ! means "this nullable variable isnt null, trust me", so dart wont throw potential null exceptions/errors
                     IconButton(
                       onPressed: () {
                         _presentDatePicker();
@@ -143,12 +194,14 @@ class _NewExpenseState extends State<NewExpense> {
             children: [
               DropdownButton(
                 value: _selectedCategory,
-                items: Category.values.map(
-                  (category) => DropdownMenuItem(
-                    value: category,
-                    child: Text(category.name.toUpperCase()),
-                  ),
-                ).toList(),
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name.toUpperCase()),
+                      ),
+                    )
+                    .toList(),
                 onChanged: (value) {
                   if (value != null) {
                     setState(() {
